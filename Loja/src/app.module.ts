209295eea171cff624488f35f91bbc5e42516169
computacaocomojota <joaovitor2004568@gmail.com>
2025-01-18 17:11:18 -0300

    Adicionando as configuracoes do TypeORM e do PostgreSQL

--- Loja/src/app.module.ts
+++ Loja/src/app.module.ts
@@ -1,8 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module} from '@nestjs/common';
 import { UsuarioModule } from './usuarios/usuario.module';
 import { ProdutoModule } from './produtos/produto.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuarioModule,ProdutoModule],
+  imports: [
+    
+    UsuarioModule,
+    ProdutoModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    })
+  ],
 })
 export class AppModule {}
